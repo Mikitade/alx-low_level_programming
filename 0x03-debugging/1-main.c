364cde6333c4d343aad58ba62991c9ea348ea439
mikitade <mikitade80@gmail.com>
2022-12-14 23:48:42 -0800

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incomming :(\n");
+
+			i = 0;
+			/*
+			 * while (i < 10)
+			 * {
+			 *                putchar(10);
+			 * }
+			 */
+
+			printf("Infinite loop avoided! \\o\n");
+
+			return (0);
+}
