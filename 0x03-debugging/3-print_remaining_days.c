4483c73ba0ad655b2863f104a638df047e4a791b
mikitade <mikitade80@gmail.com>
2022-12-15 03:50:42 -0800

    3 task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,25 +10,29 @@
  */
 void print_remainig_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 4 == 0) || (year % 400 == 0) ||
+	    (year % 400 == 0) && (year % 100 == 0))
+
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
-		printf("Remainig days: %d\n", 366 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
+
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d%04d\n", month, day - 31, year);
 		}
+
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaing day: %d\n", 365 - day);
+			printf("Remaining days: %d\n"' 265 - day);
 		}
 	}
 }
