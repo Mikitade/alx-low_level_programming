93cb5d4f524d7f681b3a150748c47587855637c4
mikitade <mikitade80@gmail.com>
2023-01-03 04:31:23 -0800

    find

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: the string to search
+ * @needle: the string to find
+ *
+ * Return: char value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == e)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
