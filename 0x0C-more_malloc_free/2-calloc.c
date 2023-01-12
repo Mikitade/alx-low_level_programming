e8b725c2f32295b6440e1c96c61d8e1151f86b22
mikitade <mikitade80@gmail.com>
2023-01-12 04:27:54 -0800

    arrar alloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _calloc - Allocates memory ffor an array of certain number
- *           of elements each off an inputted bytte size.
+ * _calloc - Allocates memory for an array of a certain number
+ *           of elements each of an inputted byte size.
  * @nmemb: The number of elements.
  * @size: The byte size of each array element.
  *
  * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         othewise - a pointer to the allocated memory.
+ *         Otherwise - a pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
